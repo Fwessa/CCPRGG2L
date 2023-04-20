ea26cb62ed0ff813aed9b879f53af8cf4cb7398d
Luisa Dala <111546558+Fwessa@users.noreply.github.com>
2023-04-20 11:55:59 +0800

    Create Book.java

---
+++ WarmUpAttendance/Book.java
@@ -0,0 +1,33 @@
+
+public class Book {
+
+	String name;
+	String author;
+	int copies;
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getCopies() {
+		return copies;
+	}
+	
+	public void setCopies(int copies) {
+		this.copies = copies;
+	}
+
+}
